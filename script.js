1e3d6beff2ea14d332c8fc59fafdea69ba1a4b0f
llee-125 <llee@berkeley.edu>
2020-04-24 11:46:00 -0700

    NOT DONE, need to fix display questions, generate password and design.

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var passwordText = document.querySelector("#password");
+  var passwordText = document.getElementById("password");
   var possibleChar = "";
   var randNum;
   var randomString = "";
 
-  // Change all your strings to arrays
   var numChar = [1,2,3,4,5,6,7,8,9,0];
   var ucChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lcChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var symbChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}"]
-
-  passwordText.value = password;
-}
-
-generateBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-})
-
-let choicesArray = [];
+  var symbChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}"];
 
 // Define generatePassword here
 function generatePassword (){
@@ -53,21 +44,50 @@ function generatePassword (){
   console.log(symb);
 
   }
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+})
+
+let choicesArray = [];
+
+  for (var i = 0; i < pwlength; i++) {
+    randNum = Math.floor(Math.random() * choicesArray.length)
+    // returns a random number from 0 to 73
+    // if randNum === 11
+    randomString += choicesArray[randNum];
+  }
+
   // On the last confirm, generate a random password based on their answers.
 
     // STEP 1 ===========================================
     // If they want numbers, include numbers
+
+    if (num === true) {
+      choicesArray = numChar;
+    }
+
+
+
+    // If (num === true && uc === true && lc === true && symb === true) 
+    // If (num === true && uc === true && lc === true && symb === false)
+    // If (num === true && uc === true && lc === false && symb === false)
+    // If (num === true && uc === false && lc === false && symb === false)
+
     // Basically if num === true {
       // then include numChar in our BIG array of choices aka choicesArray
-      choicesArray = numChar;
-      // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-    // }
+    //   choicesArray = numChar;
+    //   // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    // // }
 
-    // If they want uppercase, include uppercase characters
-    // If uc === true, then include ucChar
-      choicesArray = ucChar;
-      choiceArray = lcChar;
-      choiceArray = symbChar;
+    // // If they want uppercase, include uppercase characters
+    // // If uc === true, then include ucChar
+    //   choicesArray = ucChar;
+    //   choiceArray = lcChar;
+    //   choiceArray = symbChar;
 
     /// do the same for lc and symb
     // for symbols and numbers:
@@ -79,17 +99,14 @@ function generatePassword (){
     // so numChar + ucChar + lcChar + symbChar
 
     // STEP 2 ================================================
-  If (num === true && uc === true && lc === true && symb === true) 
-  If (num === true && uc === true && lc === true && symb === false)
-  If (num === true && uc === true && lc === false && symb === false)
-  If (num === true && uc === false && lc === false && symb === false)
+
 
 
 
   // The last line of your generatePassword function should be a return
   // return password here like
-  return "78UY$093";
-}
+  // return "78UY$093";
+// }
 
 // var passwordTest = generatePassword();
 // console.log(passwordTest);
@@ -100,13 +117,6 @@ function generatePassword (){
     // For example, if the user chooses yes to numbers and no to everything else, array looks like [1, 2, 3, 4, 5, 6, 7, 8, 9].
     // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "@" ...]
     // Once you have array compiled
-      // Pull random characters from this array based on pwlength
-      // Display this password to the page
-
-
-
-  for (let i = 0; i < pwlength; i++) {
-    randNum = Math.floor(Math.random() * arr.length)
-    randomString += choicesArray[randNum];
-  }
-document.getElementById("randomString").innerHTML = password
+    // Pull random characters from this array based on pwlength
+    // Display this password to the
+}
