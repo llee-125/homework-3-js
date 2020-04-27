361945812bde4218c41ce0a2fe817e1df3bbd145
llee-125 <llee@berkeley.edu>
2020-04-27 15:54:17 -0700

    Questions now working and logging correctly

--- readme.md
+++ readme.md
@@ -1,12 +1,11 @@
-Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
-If you are unfamiliar with special characters, take a look at some examples.
+## Password Generator
+Create an application that generates a random password based on user-selected criteria (numbers, lc letters, up letters and numbers). This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
 
-##User Story
-AS AN employee with access to sensitive data
+## User Story
 I WANT to randomly generate a password that meets certain criteria
 SO THAT I can create a strong password that provides greater security
 
-##Acceptance Criteria
+## Acceptance Criteria
 GIVEN I need a new, secure password
 WHEN I click the button to generate a password
 THEN I am presented with a series of prompts for password criteria
@@ -22,3 +21,21 @@ WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
+
+## Features
+* Used Alerts to ask the questions of what digits are requested within the password.
+* Console.log all input
+* Minimal decor on page
+
+## Improvements Needed
+* Password is not generating, need help with code.
+* Test on password character limitations if running through Alerts more than 1 time.
+
+## Contact Info.
+* Louise Lee
+* leefamilyof4@gmail.com
+
+## Credits
+* Danielle Arias--shared code to help me through coding issues.
+* TAs
+
--- script.js
+++ script.js
@@ -1,122 +1,49 @@
-// Assignment Code
-// var generateBtn = document.getElementById("#generate");
+var pwlength = prompt ("WELCOME TO PASSWORD GENERATOR -- You can have a password with 8 - 128 characters. Please enter the number of  characters you wish to use in your password?");
+console.log(pwlength);
 
-// Write password to the #password input
+if (pwlength <8 || pwlength >128) {
+  alert ("Try again, number must be between 8 - 128.")
+  prompt ("You can have a password from 8 - 128 characters. How many characters do you want your password to be?");
+} 
+
+var num = confirm ("Do you want numbers in your password?");
+console.log(num);
+
+var uc = confirm ("Do you want uppercase characters in your password?");
+console.log(uc);
+
+
+var lc = confirm ("Do you want lowercase characters in your password?");
+console.log(lc);
+
+var symb = confirm ("Do you want symbol charaters in your password?");
+console.log(symb);
+
+
+var generateBtn = document.querySelector("#generate");
+
 function writePassword() {
   var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-  var passwordText = document.getElementById("password");
-  var possibleChar = "";
-  var randNum;
-  var randomString = "";
 
+
+generateBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+  
+function generatePassword (pwlength) {
   var numChar = [1,2,3,4,5,6,7,8,9,0];
   var ucChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lcChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var symbChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}"];
 
-// Define generatePassword here
-function generatePassword (){
-  // Ask the user how long they want their password, and store it in a variable to be used later
-  var pwlength = prompt ("You can have a password from 8 - 128 characters. How many characters do you want your password to be?");
-  
-  // If the password length is less than 8, ask them to try again.
-  if (pwlength <8 || pwlength >128) {
-    alert ("Try again, number must be between 8 - 128.");
-  } 
-  // Otherwise, move onto next question.
-  else {
-    // Ask if they want numbers and store the answer in a variable to be used later.
-  var num = confirm ("Do you want numbers in your password?");
-  console.log(pwlength);
-  console.log(num);
-
-    // Ask if they want uppercase characters and store that in a variable.
-  var uc = confirm ("Do you want uppercase characters in your password?");
-  console.log(uc);
-
-    // ask if they want lowercase and store it.
-  var lc = confirm ("Do you want lowercase characters in your password?");
-  console.log(lc);
-
-    // ask if they want symbols and store it.
-  var symb = confirm ("Do you want symbol charaters in your password?");
-  console.log(symb);
-
-  }
-
-  passwordText.value = password;
 }
-
-generateBtn.addEventListener("click", function (event) {
-  event.preventDefault();
 })
+var password = "";
 
-let choicesArray = [];
+for (i = 0; i < length; i++) {
+  password += possibleCharacters.charAt(
+    Math.floor(Math.random() * possibleCharacters.length)
+  );
+}
+document.querySelector("#password").innerHTML = password;
 
-  for (var i = 0; i < pwlength; i++) {
-    randNum = Math.floor(Math.random() * choicesArray.length)
-    // returns a random number from 0 to 73
-    // if randNum === 11
-    randomString += choicesArray[randNum];
-  }
-
-  // On the last confirm, generate a random password based on their answers.
-
-    // STEP 1 ===========================================
-    // If they want numbers, include numbers
-
-    if (num === true) {
-      choicesArray = numChar;
-    }
-
-
-
-    // If (num === true && uc === true && lc === true && symb === true) 
-    // If (num === true && uc === true && lc === true && symb === false)
-    // If (num === true && uc === true && lc === false && symb === false)
-    // If (num === true && uc === false && lc === false && symb === false)
-
-    // Basically if num === true {
-      // then include numChar in our BIG array of choices aka choicesArray
-    //   choicesArray = numChar;
-    //   // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-    // // }
-
-    // // If they want uppercase, include uppercase characters
-    // // If uc === true, then include ucChar
-    //   choicesArray = ucChar;
-    //   choiceArray = lcChar;
-    //   choiceArray = symbChar;
-
-    /// do the same for lc and symb
-    // for symbols and numbers:
-    // choicesArray = numChar + symbChar
-    // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "@",]
-
-    // If they want all four, include all four
-    // If (num === true && uc === true && lc === true && symb === true )
-    // so numChar + ucChar + lcChar + symbChar
-
-    // STEP 2 ================================================
-
-
-
-
-  // The last line of your generatePassword function should be a return
-  // return password here like
-  // return "78UY$093";
-// }
-
-// var passwordTest = generatePassword();
-// console.log(passwordTest);
-
-    // Take the answers from the stored variables, compile an array with all of the choices
-// var arr = [numbchar + ucchar + lcchar + symchar] 
-
-    // For example, if the user chooses yes to numbers and no to everything else, array looks like [1, 2, 3, 4, 5, 6, 7, 8, 9].
-    // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "@" ...]
-    // Once you have array compiled
-    // Pull random characters from this array based on pwlength
-    // Display this password to the
 }
